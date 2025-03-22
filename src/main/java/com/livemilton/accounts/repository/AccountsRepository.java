558b4ce03a698740d05f60d1f0b8963643a09d22
milton munoz <livemilton@hotmail.com>
2025-03-21 22:28:44 -0500

    section2-writting spring data JPA entities and repositories

---
+++ src/main/java/com/livemilton/accounts/repository/AccountsRepository.java
@@ -0,0 +1,9 @@
+package com.livemilton.accounts.repository;
+
+import com.livemilton.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+}
