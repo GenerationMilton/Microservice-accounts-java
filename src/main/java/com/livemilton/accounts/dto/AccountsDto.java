3095314a632a083f85143b0afd28a746f46beb4d
milton munoz <livemilton@hotmail.com>
2025-03-26 20:46:15 -0500

    section2-Enhancing documentation of REST APIs using @Schema and example data part1

--- src/main/java/com/livemilton/accounts/dto/AccountsDto.java
+++ src/main/java/com/livemilton/accounts/dto/AccountsDto.java
@@ -1,19 +1,33 @@
 package com.livemilton.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name="Accounts",
+        description="Schema to hold Account information"
+)
 public class AccountsDto {
 
     @NotEmpty(message = "AccountNumber can not be a null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
+    @Schema(
+            description="Account Number of livemilton Bank account",example = "1234563212"
+    )
     private Long accountNumber;
 
     @NotEmpty(message = "AccountType can not be a null or empty")
+    @Schema(
+            description="Account Type of livemilton Bank account", example = "Savings"
+    )
     private String accountType;
 
     @NotEmpty(message = "BranchAddress can not be a null or empty")
+    @Schema(
+            description="Livemilton Bank branch address",example = "123NewYork"
+    )
     private String branchAddress;
 }
--- src/main/java/com/livemilton/accounts/dto/CustomerDto.java
+++ src/main/java/com/livemilton/accounts/dto/CustomerDto.java
@@ -1,5 +1,6 @@
 package com.livemilton.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
@@ -8,18 +9,38 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(
+        name="Customer",
+        description="Schema to hold Customer and Account information"
+)
 public class CustomerDto {
 
+    @Schema(
+            name="Name of the customer",example="Livemilton"
+
+    )
     @NotEmpty(message = "Name can not be a null or empty")
     @Size(min=5, max=30,message = "The length of the customer name should be between 5 and 30")
     private String name;
 
+    @Schema(
+            name="Email address of the customer",example="Livemilton@hotmail.com"
+
+    )
     @NotEmpty(message = "Email address can not be a null or empty")
     @Email(message = "Email address should be a valid value")
     private String email;
 
+    @Schema(
+            name="Mobile Number of the customer",example="3003778759"
+
+    )
     @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
     private String mobileNumber;
 
+    @Schema(
+            name="Account details of the Customer"
+
+    )
     private AccountsDto accountsDto;
 }
